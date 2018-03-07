bb4613f356ca3d5168d3afe843bfee1edc25fcfa
matt knox <matthewknox@gmail.com>
2018-03-06 17:08:49 -0800

    added a nice little checkin script

--- checkin.rb
+++ checkin.rb
@@ -6,9 +6,11 @@ return if PATHLIST.empty?
 
 PATHLIST.split.each do |path|
   puts "path: #{path}"
-  `cd #{path}`
-  puts `pwd`
-  `git ls-files --deleted -z | xargs -0 git rm >/dev/null 2>&1`
-  `git add .`
-  `git commit -m "checkin.rb autocommit $(date)"`
+  fn = File.expand_path(path)
+  if fn && File.exists?(fn) && File.directory?(fn)
+    Dir.chdir(fn)
+    `git ls-files --deleted -z | xargs -0 git rm >/dev/null 2>&1`
+    `git add .`
+    `git commit -m "checkin.rb autocommit $(date +'%Y%m%d_%H%M%S')"`
+  end
 end
